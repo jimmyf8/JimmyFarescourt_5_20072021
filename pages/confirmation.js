c10f7d28b5c489b170beda3800bdbc8904a712fa
jimmy <jimmy.farescourt@gmail.com>
2021-08-14 19:06:53 +0200

    modification apparence page confirmation

--- pages/confirmation.js
+++
@@ -1,5 +0,0 @@
-if(localStorage.getItem("Quantité") != null)
-h1.textContent = `Vous avez commandé ${localStorage.getItem("Quantité")} produit`;
-
-if(localStorage.getItem("Quantité") != null && localStorage.getItem("Quantité") >1)
-h1.textContent = `Vous avez commandé ${localStorage.getItem("Quantité")} produits`;
--- pages/panier.js
+++ pages/panier.js
@@ -103,3 +103,10 @@ function getTeddies(storedTeddies) {
     return result;
 }
 getStoredTeddies()
+
+//récapitulatif
+if(localStorage.getItem("Quantité") != null)
+h1.textContent = `Vous avez commandé ${localStorage.getItem("Quantité")} produit`;
+
+if(localStorage.getItem("Quantité") != null && localStorage.getItem("Quantité") >1)
+h1.textContent = `Vous avez commandé ${localStorage.getItem("Quantité")} produits`;
